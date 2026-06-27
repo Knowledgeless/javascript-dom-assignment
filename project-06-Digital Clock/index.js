f608ee2ed8744354d477adbf5593f86a0ce99777
knowledgeless <nazmulhossainxi9@gmail.com>
2026-06-27 18:28:24 +0600

    uploading digital clock

---
+++ project-06-Digital Clock/index.js
@@ -0,0 +1,19 @@
+let clock = document.querySelector("#clock");
+
+function updateClock() {
+    let now = new Date();
+
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+updateClock();
+
+setInterval(updateClock, 1000);
