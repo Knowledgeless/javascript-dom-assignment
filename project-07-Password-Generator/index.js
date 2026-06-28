e394c3c574f83c86206c0f83c3e78ac833a675cb
knowledgeless <nazmulhossainxi9@gmail.com>
2026-06-28 18:32:02 +0600

    uploading project 8

--- project-07-Password-Generator/index.js
+++ project-07-Password-Generator/index.js
@@ -6,10 +6,22 @@ let characters =
         "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.<>?";
 
 inputButton.addEventListener("click", ()=>{
-    let newPassword = ""
-    for(let i=0; i<Number(inputNumber.value); i++){
-        let randInt = Math.floor(Math.random() * characters.length);
-        newPassword += characters[randInt]
+    
+    if(inputNumber.value > 31){
+        alert("Password Lenght Must Be Less Then 30..!");
     }
-    password.innerHTML = `${newPassword}`
+    else{
+        let newPassword = ""
+        for(let i=0; i<Number(inputNumber.value); i++){
+            let randInt = Math.floor(Math.random() * characters.length);
+            newPassword += characters[randInt];
+        }
+        if(Number(newPassword.length) === Number(inputNumber.value)){
+            password.textContent = `${newPassword}`;
+        }
+        else{
+            password.innerHTML = `Try Again..!`;
+        }
+    }
+    
 })
---
+++ project-08-Gradient Background Generator/index.js
@@ -0,0 +1,37 @@
+let colorPicker1 = document.querySelector("#colorPicker1");
+let colorPicker2 = document.querySelector("#colorPicker2");
+let doneButton = document.querySelector("#doneButton");
+let colorCode1 = document.querySelector("#colorCode1");
+let colorCode2 = document.querySelector("#colorCode2");
+
+const directions = [
+    "to right",
+    "to left",
+    "to top",
+    "to bottom",
+    "45deg",
+    "90deg",
+    "135deg"
+];
+
+
+doneButton.addEventListener("click", ()=>{
+
+    let color1 = colorPicker1.value;
+    let color2 = colorPicker2.value;
+    let dir = directions[Math.floor(Math.random() * directions.length)];
+    if (color1 === "#ffffff" && color2 ==="#ffffff"){
+        document.body.style.color = "#000";
+    }
+    else if(color1 === "#000000" && color2 === "#000000"){
+        document.body.style.color = "#fff";
+    }
+    
+    document.body.style.background = `linear-gradient(${dir}, ${color1}, ${color2})`;
+
+    document.body.style.backgroundSize = "cover";
+
+    colorCode1.textContent = color1;
+    colorCode2.textContent = color2;
+    console.log(color1, color2);
+});
